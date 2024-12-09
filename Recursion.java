dce9a5e362092abb7e1867c241d3b15d7d3b3732
TanisqaRaj <rajtanisqa@gmail.com>
2024-12-10 00:26:04 +0530

    second

--- Recursion.java
+++ Recursion.java
@@ -135,28 +135,28 @@
 // }
 
 
-// import java.util.Scanner;
-// class Recursion
-// {
-//     public static int findNthFibonacci(int n) {
-//         if (n==0)
-//         {
-//             return 0;
-//         }
-//         if(n==1)
-//         {
-//             return 1;
-//         }
-//         return findNthFibonacci(n - 1) + findNthFibonacci(n - 2);
-//     }
+import java.util.Scanner;
+class Recursion
+{
+    public static int findNthFibonacci(int n) {
+        if (n==0)
+        {
+            return 0;
+        }
+        if(n==1)
+        {
+            return 1;
+        }
+        return findNthFibonacci(n - 1) + findNthFibonacci(n - 2);
+    }
 
-//     public static void main(String[] args)
-//     {
-//         Scanner scanner = new Scanner(System.in);
-//         System.out.print("Enter the value of n: ");
-//         int n = scanner.nextInt();
-//         int result = findNthFibonacci(n);
-//         System.out.println("The " + n + "th Fibonacci number is: " + result);
-//     }
-// }
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the value of n: ");
+        int n = scanner.nextInt();
+        int result = findNthFibonacci(n);
+        System.out.println("The " + n + "th Fibonacci number is: " + result);
+    }
+}
 
